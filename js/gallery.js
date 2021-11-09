a499e6eb41e4e7525f0a25e99f703e96e63965d8
Marjuly <122mcruz@frhsd.com>
2021-11-09 14:01:15 -0500

    completed pt 2 (.1 and .2)

--- js/gallery.js
+++ js/gallery.js
@@ -1,15 +1,15 @@
 // requestAnim shim layer by Paul Irish
     window.requestAnimFrame = (function(){
-      return  window.requestAnimationFrame       || 
-              window.webkitRequestAnimationFrame || 
-              window.mozRequestAnimationFrame    || 
-              window.oRequestAnimationFrame      || 
-              window.msRequestAnimationFrame     || 
+      return  window.requestAnimationFrame       ||
+              window.webkitRequestAnimationFrame ||
+              window.mozRequestAnimationFrame    ||
+              window.oRequestAnimationFrame      ||
+              window.msRequestAnimationFrame     ||
               function(/* function */ callback, /* DOMElement */ element){
                 window.setTimeout(callback, 1000 / 60);
               };
     })();
-  
+
 
 // example code from mr doob : http://mrdoob.com/lab/javascript/requestanimationframe/
 
@@ -35,7 +35,7 @@ function animate() {
 function swapPhoto() {
 	//Add code here to access the #slideShow element.
 	//Access the img element and replace its source
-	//with a new image from your images array which is loaded 
+	//with a new image from your images array which is loaded
 	//from the JSON string
 	console.log('swap photo');
 }
@@ -45,6 +45,20 @@ var mCurrentIndex = 0;
 
 // XMLHttpRequest variable
 var mRequest = new XMLHttpRequest();
+mRequest.addEventListener("readystatechange", () => {
+  //(console.log(request, request.readyChange))
+
+  if (mRequest.readyState === 4 && mRequest.status === 200) {
+    const data = JSON.parse(mRequest.responseText);
+    console.log(data);
+  } else if (mRequest.readyState === 4) {
+    console.log("could not fetch the data");
+  }
+});
+
+request.open("GET", "../images.json");
+request.send();
+
 
 // Array holding GalleryImage objects (see below).
 var mImages = [];
@@ -67,22 +81,21 @@ function makeGalleryImageOnloadCallback(galleryImage) {
 }
 
 $(document).ready( function() {
-	
+
 	// This initially hides the photos' metadata information
 	$('.details').eq(0).hide();
-	
+
 });
 
 window.addEventListener('load', function() {
-	
+
 	console.log('window loaded');
 
 }, false);
 
 function GalleryImage() {
-	//implement me as an object to hold the following data about an image:
-	//1. location where photo was taken
-	//2. description of photo
-	//3. the date when the photo was taken
-	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
+  this.location;
+  this.description;
+  this.date;
+  this.img;
 }
